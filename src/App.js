319bec43a68c8f16c22a8269c200e8001c96bf80
uttammoradiya <uttammoradiya@knovator.in>
2020-02-03 22:07:33 +0530

    Simple boiler plate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "antd";
+// const ipc = require('electron').ipcRenderer;
+// import {ipcRenderer as ipc} from 'electron'
+// const ipc = window.require("electron").ipcRenderer;
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handelOnClick = () => {
+    // console.log("this is a log in a button Click :->", name);
+    // ipc.send("create-user");
+    // ipc.on("a", (event, data) => {
+    //   console.log(data, "iopssss");
+    // });
+  };
+
+  const handelText = e => {
+    setName(e.target.value);
+    // console.log("TCL: handelText -> e.target.value", e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Name</p>
+      <Button>Antd</Button>
+      <input type="text" value={name} onChange={handelText} />
+      <p>Add</p>
+      <input />
+      <button onClick={handelOnClick}>Click!</button>
     </div>
   );
 }
