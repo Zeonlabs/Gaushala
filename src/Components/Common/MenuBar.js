45bcb8dff11432ab76f549c2d646c19e0f9778d3
uttammoradiya <uttammoradiya@knovator.in>
2020-02-05 00:47:22 +0530

    Income Module complete

--- src/Components/Common/MenuBar.js
+++ src/Components/Common/MenuBar.js
@@ -1,10 +1,11 @@
-import { Layout, Menu, Icon } from "antd";
+import { Layout, Menu, Icon, Button, Modal } from "antd";
 
 import React, { Component } from "react";
 import "../../css/Home/Menubar.css";
 import { withRouter } from "react-router";
 import routes from "../../js/Routes";
 import { Link } from "react-router-dom";
+import FormModel from "./FormModel";
 const { Header, Content, Footer, Sider } = Layout;
 
 class MenuBar extends Component {
@@ -12,9 +13,34 @@ class MenuBar extends Component {
     super(props);
 
     this.state = {
-      collapsed: false
+      collapsed: false,
+      loading: false,
+      visible: false
     };
   }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handelModel = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
   toggle = () => {
     this.setState({
@@ -29,64 +55,71 @@ class MenuBar extends Component {
           <div className="logo">
             <h2>સીતારામ ગો શાળા</h2>
           </div>
-          <Menu theme="dark" mode="inline" selectedKeys={[this.props.location.pathname]}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={[this.props.location.pathname]}
+          >
             <Menu.Item key={routes.dashboard}>
               <Link to={routes.dashboard}>
-              <Icon type="dashboard" />
-              <span>Dashboard</span>
+                <Icon type="dashboard" />
+                <span>Dashboard</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.charity}>
               <Link to={routes.charity}>
-              <Icon type="fund" />
-              <span>Charity income</span>
+                <Icon type="fund" />
+                <span>Charity income</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.expences}>
               <Link to={routes.expences}>
-              <Icon type="file-done" />
-              <span>Expenses</span>
+                <Icon type="file-done" />
+                <span>Expenses</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.cheques}>
               <Link to={routes.cheques}>
-              <Icon type="schedule" />
-              <span>Cheques</span>
+                <Icon type="schedule" />
+                <span>Cheques</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.animal}>
               <Link to={routes.animal}>
-              <Icon type="qq" />
-              <span>Animal</span>
+                <Icon type="qq" />
+                <span>Animal</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.employees}>
               <Link to={routes.employees}>
-              <Icon type="team" />
-              <span>Employees</span>
+                <Icon type="team" />
+                <span>Employees</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.notes}>
               <Link to={routes.notes}>
-              <Icon type="file-text" />
-              <span>Notes</span>
+                <Icon type="file-text" />
+                <span>Notes</span>
               </Link>
             </Menu.Item>
             <Menu.Item key={routes.trustmembers}>
               <Link to={routes.trustmembers}>
-              <Icon type="user" />
-              <span>Trust Members</span>
+                <Icon type="user" />
+                <span>Trust Members</span>
               </Link>
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: "#fff", padding: 0 }}>
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
-              onClick={this.toggle}
-            />
+          <Header className="header-css">
+            <Button
+              className="plus-button-dashboard"
+              type="primary"
+              size="large"
+              onClick={this.handelModel}
+            >
+              <Icon type="plus" />
+            </Button>
           </Header>
           <Content
             style={{
@@ -96,7 +129,19 @@ class MenuBar extends Component {
               minHeight: "calc(100vh - 112px)"
             }}
           >
-            {this.props.children}
+            <>
+              {this.props.children}
+              {this.state.visible ? (
+                <FormModel
+                  showModal={this.state.visible}
+                  loading={this.state.loading}
+                  handleOk={this.handleOk}
+                  handleCancel={this.handleCancel}
+                />
+              ) : (
+                ""
+              )}
+            </>
           </Content>
         </Layout>
       </Layout>
