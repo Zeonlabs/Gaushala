f4a4832034952e11e18ef0bf72cfe588760ab822
Ashish <ramaniakhil094@gmail.com>
2020-03-27 22:36:59 +1100

    dashboard page implemented

--- src/Components/Dashboard/AnimalChart.js
+++ src/Components/Dashboard/AnimalChart.js
@@ -1,85 +1,72 @@
 import React, { Component } from "react";
-import CanvasJSReact from "../../assets/canvasjs.react";
 import { connect } from "react-redux";
 import { getAnimalChart } from "../../Actions/ChartActions";
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+import {PieChart} from './nivoPieChart'
+
 class AnimalChart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animalData: [{ x: 0, y: 0 }],
+      animalData: [{ id: '', label: 'chart', value: 1 }],
       expenceData: [{ x: 0, y: 0 }]
     };
   }
+  
   componentDidMount = () => {
     this.props.getAnimalChart().then(res => {
       console.log("LineChart -> componentDidMount -> res", res);
       const ArrayMap = Object.values(res.stats.animal);
-      const data = [
+
+      const newData = [
         {
-          name: "Gay",
-          y: ArrayMap[0]
+          id: 'gay',
+          label: "Gay",
+          value: ArrayMap[0],
+          color: "#F9C501"
         },
         {
-          name: "Balad",
-          y: ArrayMap[1]
+          id: 'bald',
+          label: "Bald",
+          value: ArrayMap[1],
+          color: "#53D767"
         },
         {
-          name: "Vacharda",
-          y: ArrayMap[2]
+          id: 'vachrda',
+          label: "Vacharda",
+          value: ArrayMap[2],
+          color: "#021322"
         },
         {
-          name: "Vachardi",
-          y: ArrayMap[3]
+          id: 'vachardi',
+          label: "Vachardi",
+          value: ArrayMap[3],
+          color: "#40a9ff"
         },
         {
-          name: "Other",
-          y: ArrayMap[4]
+          id: 'other',
+          label: "Other",
+          value: ArrayMap[4],
+          color: "#ff4d4f"
         }
-        //  {
-        //    name : 'Gay', y:ArrayMap[5]
-        //  }
-      ];
-      console.log("AnimalChart -> componentDidMount -> data", data);
+      ]
+
       this.setState({
-        animalData: data
+        animalData: newData
       });
     });
   };
+
+
   render() {
-    const options = {
-      animationEnabled: true,
-      title: {
-        // text: "Customer Satisfaction"
-      },
-      subtitles: [
-        {
-          // text: "71% Positive",
-          verticalAlign: "center",
-          fontSize: 24,
-          dockInsidePlotArea: true
-        }
-      ],
-      data: [
-        {
-          type: "doughnut",
-          showInLegend: true,
-          indexLabel: "{name}: {y}",
-          yValueFormatString: "#,###",
-          dataPoints: this.state.animalData.map(d => ({ name: d.name, y: d.y }))
-        }
-      ]
-    };
 
     return (
-      <div>
-        <CanvasJSChart
-          options={options}
-          /* onRef={ref => this.chart = ref} */
+      <div className='pie-chrt' >
+        <PieChart 
+          data={this.state.animalData}
         />
-        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Components/Dashboard/nivoPieChart.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {ResponsivePie} from '@nivo/pie'
+
+export const PieChart = ({ data }) => (
+    <ResponsivePie
+        data={data}
+        margin={{ top: 5, right: 80, bottom: 80, left: 0 }}
+        startAngle={1}
+        innerRadius={0.7}
+        padAngle={0.9}
+        // cornerRadius={3}
+        colors={d => d.color}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{ from: 'color' }}
+        // slicesLabelsSkipAngle={10}
+        // slicesLabelsTextColor="#333333"
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+
+        legends={[
+            {
+                anchor: 'right',
+                direction: 'column',
+                // translateX: 30,
+                itemWidth: 100,
+                itemHeight: 40,
+                symbolSize: 20,
+                symbolShape: 'circle'
+            }
+        ]}
+    />
+)
